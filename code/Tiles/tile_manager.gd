840e82ba804236b315789c25fab87040ce54ab23
Joshua <jsiderius0@gmail.com>
2025-04-16 00:54:53 -0400

    Docs for tile manager

--- code/Tiles/tile_manager.gd
+++ code/Tiles/tile_manager.gd
@@ -12,7 +12,7 @@ var complete : bool = false
 var square_size : float
 
 func _init(id_grid : Grid) -> void:
-	print(id_grid)
+	
 	height = id_grid.height
 	width = id_grid.width
 	square_size = id_grid.square_size
@@ -21,47 +21,58 @@ func _init(id_grid : Grid) -> void:
 	generate_tile_grid(id_grid)
 
 func generate_tile_grid(id_grid : Grid) -> void:
-	
+	'''
+		Purpose:
+			Setup a tile grid according to the data in the Grid argument
+
+		Arguments:
+			id_grid:
+				Grid containing data for the tiles
+
+		Return: void
+	'''
+
 	if not id_grid.district_manager:
+		id_grid.update_district_manager()
 		print_debug("Grid does not contain district manager")
 		push_error("Grid does not contain district manager")
-		
+	
+	# Construct an array of all border water cells
 	var water_border : Array[Vector2i] = []
 	if id_grid.district_manager.has_district(Enums.Cell.WATER):
 		water_border = id_grid.district_manager.get_district(Enums.Cell.WATER).border
 
+	# Iterate the grid and create a tile for each cell
 	for y in range(height): 
 		tile_grid.append([])
 		for x in range(width):
 			var cell_id : int = id_grid.index(y,x)
-			if Vector2i(y, x) in water_border: cell_id = Enums.Cell.WATER_BORDER
+			if Vector2i(y, x) in water_border: cell_id = Enums.Cell.WATER_BORDER # Override cell_id with WATER_BORDER for WFC
 			tile_grid[y].append(Tile.new(cell_id))
 	
 	for y in range(height): for x in range(width): 
 		var tile : Tile = tile_grid[y][x]
 		var tile_possibilities = tile.get_possibilities()
 		
+		# Initialize NESW Neighbors and Borders
 		if y > 0:
 			tile.add_neighbor(wfcConfig.Dir.NORTH, tile_grid[y-1][x])
 			tile_grid[y-1][x].constrain(tile_possibilities, wfcConfig.Dir.NORTH)
 			tile.constrain(tile_grid[y-1][x].possibilities, wfcConfig.Dir.SOUTH)
 		else: 
 			tile.add_border(wfcConfig.Dir.NORTH)
-
 		if y < height-1:
 			tile.add_neighbor(wfcConfig.Dir.SOUTH, tile_grid[y+1][x])
 			tile_grid[y+1][x].constrain(tile_possibilities, wfcConfig.Dir.SOUTH)
 			tile.constrain(tile_grid[y+1][x].possibilities, wfcConfig.Dir.NORTH)
 		else: 
 			tile.add_border(wfcConfig.Dir.SOUTH)
-
 		if x > 0:
 			tile.add_neighbor(wfcConfig.Dir.WEST, tile_grid[y][x-1])
 			tile_grid[y][x-1].constrain(tile_possibilities, wfcConfig.Dir.WEST)
 			tile.constrain(tile_grid[y][x-1].possibilities, wfcConfig.Dir.EAST)
 		else: 
 			tile.add_border(wfcConfig.Dir.WEST)
-
 		if x < width - 1:
 			tile.add_neighbor(wfcConfig.Dir.EAST, tile_grid[y][x+1])
 			tile_grid[y][x+1].constrain(tile_possibilities, wfcConfig.Dir.EAST)
@@ -69,82 +80,118 @@ func generate_tile_grid(id_grid : Grid) -> void:
 		else: 
 			tile.add_border(wfcConfig.Dir.EAST)
 
+func get_entropy(y : int, x : int) -> int:
+	''' Returns the entropy of the tile at (y, x)'''
 
-func get_entropy(x : int, y : int) -> int:
 	return tile_grid[y][x].get_entropy()
 
-func get_type(x : int, y : int) -> int: 
-	if len(tile_grid[y][x].get_possibilities()) == 0: return wfcConfig.TILE_ERROR
-	return tile_grid[y][x].get_possibilities()[0] # TODO: ??? Should we check that the entropy is also 0? 
+func get_type(y : int, x : int) -> int:
+	''' Return the tile type of the tile at position y, x '''
+
+	if len(tile_grid[y][x].get_possibilities()) == 0:
+		print_debug("Tile type requested for tile with no possibilities")
+		push_warning("Tile type requested for tile with no possibilities")
+		return wfcConfig.TILE_ERROR
+
+	return tile_grid[y][x].get_possibilities()[0]
 
 func get_lowest_entropy_value() -> int: 
+	''' Returns the lowest (non 1/0) entropy in the grid '''
+
 	var lowest_entropy : int = len(wfcConfig.tile_edges.keys())
 	for y in range(height): for x in range(width):
-		if tile_grid[y][x].get_entropy() <= 0: continue
-		lowest_entropy = min(lowest_entropy, tile_grid[y][x].get_entropy())
+		if get_entropy(y, x) <= 1: continue
+		lowest_entropy = min(lowest_entropy, get_entropy(y, x))
 	
 	return lowest_entropy
 
 func get_lowest_entropy_tile_list() -> Array[Tile]:
+	'''
+		Purpose: 
+			Compiles and returns an array of all the lowest entropy tiles in the grid
+
+		Return: 
+			Array[Tile]: All tiles having the lowest entropy value in the grid
+	'''
 	# TODO: A priority queue could help runtime
 	
+	# Initialize variables
 	var lowest_entropy_tiles : Array[Tile] = []
 	var lowest_entropy : int = wfcConfig.TileType.size()
 	
+	# Iterate the grid
 	for y in range(height): for x in range(width): 
-		var tile_entropy : int = tile_grid[y][x].get_entropy()
-		if tile_entropy <= 0: 
+		var tile_entropy : int = get_entropy(y, x)
+		
+		# Skip solved cells and entropy larger than lowest
+		if tile_entropy <= 1 or tile_entropy > lowest_entropy:
 			continue
-		elif tile_entropy > lowest_entropy: 
-			continue
+		# Add to array if matching entropy
 		elif tile_entropy == lowest_entropy: 
 			lowest_entropy_tiles.append(tile_grid[y][x])
 			continue
+		# Reset variables if lower entropy
 		else:
 			lowest_entropy = tile_entropy
 			lowest_entropy_tiles = [tile_grid[y][x]]
 	
 	return lowest_entropy_tiles
 
-func wave_function_collapse(dm : DistrictManager) -> void:
-	
+func wave_function_collapse() -> void:
+	''' Execute the wave function collapse algorithm '''
+
+	# Run iterations of the algorithm until complete
 	var is_complete : bool = false
 	while not is_complete: 
-		is_complete = await wave_function_collapse_iteration()
+		is_complete = wave_function_collapse_iteration()
 		await redraw_and_pause(-1, 0.0, false)
 
 	complete = true
-	print("Block 1 time : ", block_1_time / (block_1_time + block_2_time), "%")
-	print("Block 2 time : ", block_2_time / (block_1_time + block_2_time), "%")
+	print_debug("Block 1 time : ", block_1_time / (block_1_time + block_2_time), "%")
+	print_debug("Block 2 time : ", block_2_time / (block_1_time + block_2_time), "%")
 	
-var block_1_time : float = 0 # about 8%
-var block_2_time : float = 0 # about 92%
+var block_1_time : float = 0 # about %
+var block_2_time : float = 0 # about %
 func wave_function_collapse_iteration() -> bool:
-	
+	'''
+		Purpose: 
+			Completes a single iteration of the wave function collapse algorithm
+		Return:
+			bool: indicates if the algorithm is complete
+	'''
+
 	var start_1 = Time.get_ticks_usec()
 
-	var lowest_entropy_tiles : Array[Tile] = get_lowest_entropy_tile_list() # This could be a long time thing 
-	if len(lowest_entropy_tiles) == 0:
+	# Get an array containing all tiles of the lowest entropy
+	var lowest_entropy_tiles : Array[Tile] = get_lowest_entropy_tile_list()
+
+	# Exit if all cells have selected a tile type
+	if len(lowest_entropy_tiles) <= 1:
 		return true
 	
 	block_1_time += Time.get_ticks_usec() - start_1
 
+	# Select a tile randomly from the lowest entropy cells and collapse it
 	var tile_to_collapse : Tile = lowest_entropy_tiles[randi() % len(lowest_entropy_tiles)]
 	tile_to_collapse.collapse()
 	
 	var start_2 = Time.get_ticks_usec()
 
+
+	# Constrain all tiles effected by the collapse
 	var stack : Array = [tile_to_collapse]
 	while(len(stack) > 0):
 		var tile : Tile = stack.pop_back()
-		var tile_possibilities = tile.get_possibilities()
-		var tile_directions = tile.get_directions()
 		
-		for direction in tile_directions:
+		# Iterate the tiles neighbors
+		for direction in tile.get_directions():
+
+			# Setup the neighbor
 			var neighbor : Tile = tile.get_neighbor(direction)
 			if neighbor.get_entropy() == 0: continue
 			
-			if neighbor.constrain(tile_possibilities, direction): 
+			# Constrain the neighbor according to the tile, if it reduces add it to the stack
+			if neighbor.constrain(tile.get_possibilities(), direction): 
 				stack.append(neighbor)
 				
 	block_2_time += Time.get_ticks_usec() - start_2
